a9ba526d9833029c50786434b62d77a137e9c0a0
Margaret <maenunezt@gmail.com>
2023-08-15 15:26:53 -0400

    creación del proyecto

---
+++ app/src/main/java/com/example/sprint6/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.sprint6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/* Lista para una aplicación que utiliza información de una API y que contiene imágenes.
+[ ] Permiso de internet y clear traffic (o algo así) en Manifest
+[ ] ViewBinding
+[ ] Dependencias: Coil, Room, Retrofit, navigation, viewModel. Agregar el plugin de ROOM y cambiar la versión de la 1.8 a la 17
+[ ] Crear package data, data.local, data.remote y presentation(o vistas)
+[ ] Remoto:
+    [ ] Dataclass/ POJO
+    [ ] API (interfaz)
+    [ ] Cliente de Retrofit
+[ ] Repositorio
+[ ] Local:
+    [ ] Entity
+    [ ] DAO
+    [ ] Database
+[ ] View Model
+[ ] 2 pantallas: Listado y detalle
+[ ] Navigation
+[ ] Listado: RecyclerView + ViewHolder + Adapter
+[ ] Detalle: RecyclerView + ViewHolder + Adapter
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
