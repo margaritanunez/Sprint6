59190ddc603a5e80c3c8ef4e29c8eef30dc4a3c8
Margaret <maenunezt@gmail.com>
2023-08-15 15:53:04 -0400

    se agregan permisos de internet y usesCleartextTraffic en AndroidManifest.xml

--- app/src/main/java/com/example/sprint6/MainActivity.kt
+++ app/src/main/java/com/example/sprint6/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 /* Lista para una aplicación que utiliza información de una API y que contiene imágenes.
-[ ] Permiso de internet y clear traffic (o algo así) en Manifest
+[x] Permiso de internet y clear traffic (o algo así) en Manifest
 [x] ViewBinding
 [x] Dependencias: Coil, Room, Retrofit, navigation, viewModel y Test de android. Agregar el plugin de ROOM y cambiar la versión de la 1.8 a la 17
 [ ] Crear package data, data.local, data.remote y presentation(o vistas)
