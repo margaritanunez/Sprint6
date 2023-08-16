c3474937a787ca9e2c8e4fea48322f63fe7c895f
Margaret <maenunezt@gmail.com>
2023-08-16 16:51:23 -0400

    creación de DetailFragment.kt

--- app/src/main/java/com/example/sprint6/presentation/DetailFragment.kt
+++ app/src/main/java/com/example/sprint6/presentation/DetailFragment.kt
@@ -5,28 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.sprint6.R
+import com.example.sprint6.databinding.FragmentDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM1 = "id"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
+    private lateinit var binding : FragmentDetailBinding
+    private val phoneViewModel: PhoneViewModel by activityViewModels()
     // TODO: Rename and change types of parameters
     private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -35,26 +29,18 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        initAdapterDetalle()
+        phoneViewModel.getDetailPhoneViewModel(param1!!.toInt())
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initAdapterDetalle() {
+        val adapterDetail = AdapterDetail()
+        binding.rvDetail.adapter= adapterDetail
+        phoneViewModel.detailLiveData(param1!!.toInt()).observe(viewLifecycleOwner){
+            adapterDetail.setDataDetalle(it)
+        }
     }
+
 }
